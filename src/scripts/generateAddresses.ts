1a8e2593ec073cc15e16f527f5b8b93d36a99ca7
Baptiste Marchand <75846779+baptiste-marchand@users.noreply.github.com>
2025-11-05 16:41:49 +0100

    refactor: remove dead code

--- src/scripts/generateAddresses.ts
+++
@@ -1,7 +0,0 @@
-import { Keypair } from '@solana/web3.js';
-
-export const generateRandomAddresses = (): string[] => {
-  return Array.from({ length: 10 }, () => Keypair.generate().publicKey.toBase58());
-};
-
-console.log(generateRandomAddresses());
