8fe07b3efa2487f409eb829fd9592420fa6ea7f4
NikosFotiadis <nikosfotiadis0@gmail.com>
2025-12-12 16:25:25 +0200

    feat: sign transaction with standard

--- src/hooks/useSignTransaction.ts
+++ src/hooks/useSignTransaction.ts
@@ -1,17 +1,59 @@
 import { useCallback } from 'react';
 import { BitcoinNetworkType, type InputToSign, signTransaction } from 'sats-connect';
+import { Psbt } from 'bitcoinjs-lib';
 import { useEndpoint } from '../context/EndpointProvider';
+import { WalletConnectionType } from '../context/BitcoinWalletProvider';
+import {
+  BitcoinSignTransaction,
+  BitcoinSignTransactionFeature
+} from '../features/signTransaction'
 import { useConnect } from './useConnect';
 
 export function useSignTransaction() {
   const { network } = useEndpoint();
-  const { selectedAccount, statsConnectProvider } = useConnect();
+  const {
+    selectedAccount,
+    statsConnectProvider,
+    selectedConnectionType,
+    selectedWallet
+  } = useConnect();
 
-  return useCallback(
+  const signTransactionWithStandard = useCallback(
     async (psbtBase64: string, message: string, inputsToSign: InputToSign[] = [], broadcast?: boolean) => {
-      if (!selectedAccount) {
+      if (!selectedWallet) {
         throw new Error('Wallet not connected');
       }
+      if (!selectedAccount) {
+        throw new Error('Account not selected');
+      }
+
+      const signTransactionFeature = selectedWallet.features[BitcoinSignTransaction] as
+        | BitcoinSignTransactionFeature[typeof BitcoinSignTransaction]
+        | undefined;
+      if (!signTransactionFeature) {
+        throw new Error('Wallet does not support message signing');
+      }
+
+      const result = await signTransactionFeature.signTransaction({
+        psbt: Buffer.from(psbtBase64, 'base64'),
+        inputsToSign: inputsToSign.map((input) => ({
+          account: selectedAccount,
+          signingIndexes: input.signingIndexes,
+          sigHash: "ALL",
+        })),
+        chain: network,
+      });
+
+      return {
+        psbtBase64: Buffer.from(result[0].signedPsbt.buffer).toString('base64'),
+        txId: '',
+      }
+    },
+    [selectedAccount, statsConnectProvider, network],
+  );
+
+  const signTransactionWithSatsConnect = useCallback(
+    async (psbtBase64: string, message: string, inputsToSign: InputToSign[] = [], broadcast?: boolean) => {
       if (!statsConnectProvider) {
         throw new Error('Sats Connect provider not available');
       }
@@ -31,6 +73,28 @@ export function useSignTransaction() {
         }),
       );
       return res as { psbtBase64: string; txId?: string };
+    },
+    [selectedAccount, statsConnectProvider, network],
+  );
+
+  return useCallback(
+    async (psbtBase64: string, message: string, inputsToSign: InputToSign[] = [], broadcast?: boolean) => {
+      if (!selectedAccount) {
+        throw new Error('Wallet not connected');
+      }
+     
+      if (!selectedConnectionType) {
+        throw new Error('Connection type not selected');
+      }
+
+      switch (selectedConnectionType) {
+        case WalletConnectionType.Standard:
+          return signTransactionWithStandard(psbtBase64, message, inputsToSign);
+        case WalletConnectionType.SatsConnect:
+          return signTransactionWithSatsConnect(psbtBase64, message, inputsToSign);
+        default:
+          throw new Error(`Unsupported connection type: ${selectedConnectionType}`);
+      }
     },
     [selectedAccount, statsConnectProvider, network],
   );
