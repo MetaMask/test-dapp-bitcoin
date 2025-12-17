646fec9fc53eaee242c5aeda11ec89fc6573ed7b
NikosFotiadis <nikosfotiadis0@gmail.com>
2025-12-17 19:10:49 +0200

    chore: remove .js from exports

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,11 +1,11 @@
 import type { WalletWithFeatures } from '@wallet-standard/base';
 
-import type { BitcoinConnectFeature } from './connect.js';
-import type { BitcoinDisconnectFeature } from './disconnect.js';
+import type { BitcoinConnectFeature } from './connect';
+import type { BitcoinDisconnectFeature } from './disconnect';
 import type { BitcoinSatsConnectFeature } from './satsConnect.js';
-import type { BitcoinSignAndSendTransactionFeature } from './signAndSendTransaction.js';
-import type { BitcoinSignMessageFeature } from './signMessage.js';
-import type { BitcoinSignTransactionFeature } from './signTransaction.js';
+import type { BitcoinSignAndSendTransactionFeature } from './signAndSendTransaction';
+import type { BitcoinSignMessageFeature } from './signMessage';
+import type { BitcoinSignTransactionFeature } from './signTransaction';
 
 /** Type alias for some or all Bitcoin features. */
 export type BitcoinStandardFeatures = BitcoinConnectFeature &
@@ -20,10 +20,10 @@ export type WalletWithBitcoinStandardFeatures = WalletWithFeatures<BitcoinStanda
 /** Wallet with Bitcoin statsConnect feature. */
 export type WalletWithBitcoinSatsConnectFeature = WalletWithFeatures<BitcoinSatsConnectFeature>;
 
-export * from './connect.js';
-export * from './disconnect.js';
-export * from './signTransaction.js';
-export * from './signAndSendTransaction.js';
-export * from './signMessage.js';
-export * from './satsConnect.js';
-export * from './utils.js';
+export * from './connect';
+export * from './disconnect';
+export * from './signTransaction';
+export * from './signAndSendTransaction';
+export * from './signMessage';
+export * from './satsConnect';
+export * from './utils';
